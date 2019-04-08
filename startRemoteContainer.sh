166da0427c7ceb34c1bd7ae8a00e977cd8a61290
Ben Nelson <bknelson4@gmail.com>
2019-04-07 23:03:58 -0700

    Removing remote server script

--- startRemoteContainer.sh
+++
@@ -1,9 +0,0 @@
-sudo docker run -it -d \
-    -p 17171:17171 \
-    -p 17179:17179 \
-    --add-host xmage.ndjinn.net:0.0.0.0 \
-    -m 2g \
-    -c 512 \
-    -e "XMAGE_DOCKER_SERVER_ADDRESS=xmage.ndjinn.net" \
-    -e "XMAGE_DOCKER_SERVER_NAME=xmage-server" \
-    bknelson/xmage:latest
